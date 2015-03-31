a675b0c1384725779ac9d38eecc42c7a9813299d
Spence Konde <spencekonde@gmail.com>
2015-03-31 16:27:24 -0400

    Update easyvr_democode.js

--- easyvr_democode.js
+++ easyvr_democode.js
@@ -23,10 +23,25 @@ var ocm=function(menu,option) {
   } else {
     if (menu==2) { // toggle a fargo or RF controlled device
       console.log("toggle device "+option);
+      if (option < 8) {
+        setFargo(option,!fargo[option]);
+      } 
     } else if (menu==3) { // control desk lamp
       console.log("desk lamp "+option);
     } else if (menu==4) { // control nixie clock
-      console.log("control nixie clock "+option);
+      if (option==0) { //clock on
+        setDesk("nixs=1;uplcd();")
+      } else if (option==1) { //clock off
+        setDesk("nixs=0;uplcd();")
+      } else if (option==2) { //time
+        setDesk("nixs=1;MnuS=0;Mnu0=0;uplcd();")
+      } else if (option==3) { //temp
+        setDesk("nixs=1;MnuS=0;Mnu0=1;uplcd();")
+      } else if (option==4) { //humidity
+        setDesk("nixs=1;MnuS=0;Mnu0=2;uplcd();")
+      //} else if (option==5) { //pressure
+      //  setDesk("nixs=1;MnuS=0;Mnu0=3;uplcd();")
+      } 
     }
     digitalWrite(LED1,0);
     return {type:1,timeout:0};
@@ -45,3 +60,51 @@ Serial4.setup(9600,{tx:C10,rx:C11});
     
 var evr=require("easyvr").connect(Serial4,ocm,otm,otm);
 
+
+//My standard fargo network commands
+fargosturl="http://192.168.2.12/fargostatus.php";
+dateurl="http://192.168.2.12/date.php";
+fargourl="http://192.168.2.14/api/relay/";
+deskurl="http://192.168.2.16/code.run"
+fargo=new Uint8Array(8);
+
+function setDesk(command) {
+	require("http").get(fargourl+(relay+1).toString()+postfix, function(res) {
+		res.on('close',function () {
+			if(this.code!=200) {
+				console.log("Error commanding desklamp/nixie: "+this.code);
+			}
+		});
+	});
+}
+
+
+function getFargostatus() {
+	var fargost="";
+	require("http").get(fargosturl, function(res) {
+		res.on('data',function (data) {fargost+=data;});
+		res.on('close',function() {var tfs=JSON.parse(fargost); vtfs=tfs; for (var i=0;i<8;i++) { fargo[i]=tfs.relaystate[i].state;} if(MnuS==3){uplcd(1);}});
+	});
+}
+
+function setFargo(relay,state) {
+	var postfix = (state) ? "/on":"/off";
+	require("http").get(fargourl+(relay+1).toString()+postfix, function(res) {
+		res.on('close',function () {
+			if(this.code!=200) {
+				fargo[relay]=state;
+			}
+		});
+	});
+}
+
+
+
+function getDate() {
+	var date="";
+	require("http").get(dateurl, function(res) {
+		res.on('data',function (data) {date+=data;});
+		res.on('close',function() {clk=new Clock(date);});
+	});
+	//delete getDate;
+}
