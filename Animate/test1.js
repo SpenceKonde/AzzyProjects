0cd8e3d5bf7fc17fabb83e336f4af18e25d9713e
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2016-01-22 13:47:53 -0500

    Update test1.js

--- Animate/test1.js
+++ Animate/test1.js
@@ -1,1 +1,11 @@
 
+I2C1.setup({sda:B6,sda:B7});
+var eep=require("AT24").connect(I2C1,...);
+
+SPI1.setup(...) //set up SPI1 for APA102;
+
+function APA102Lite(data){
+  SPI1.send(data);
+  SPI1.send([0xFF,0xFF,0xFF,0xFF]);
+}
+
