83d016166b885a8e07605de329eaca115f70ac9e
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2016-01-22 18:29:01 -0500

    Update test1.js

--- Animate/test1.js
+++ Animate/test1.js
@@ -8,4 +8,15 @@ function APA102Lite(data){
   SPI1.send(data);
   SPI1.send([0xFF,0xFF,0xFF,0xFF]);
 }
-
+var frameNumber =0;
+var ledCount=5;
+var offset=0x100;
+var frameCount = 10;
+function onTimeout() {
+  data=eep.read(offset+(frameNumber)*(ledCount*4+4), (ledCount*4+4));
+  var dly=data[3]+(data[2]<<8); 
+  dly=20;
+  data[0]=0; data[1]=0; data[2]=0; data[3]=0;
+  APA102Lite(data);
+  setTimeout("onTimeout()",dly);
+}
