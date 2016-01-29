b2489bda64d25a8d87eb321e7d00567d9a858400
Spence Konde <spencekonde@gmail.com>
2016-01-29 10:00:08 -0500

    qwe

--- Animate/test2.js
+++ Animate/test2.js
@@ -245,6 +245,90 @@ leds.flip = function () {
 	this.spi.write(0,0,0,0,this.fbuf,0xFF,0xFF,0xFF,0xFF);
 };
 
+leds.flip = function () {
+	var tclb=new Uint8ClampedArray(this.num*3);
+	//var x=new Uint16Array(3);
+	for (var i=0;i<(this.num*3);i++) {
+      tclb[i]=leds.buff[i]+(leds.buff[i]?leds.twinkle[i]:0)+leds.overlay[i];
+	}
+  //console.log(tclb);
+	for (var i=0;i<numleds;i++) {
+		//var x = new Uint16Array([tclb[i*3]<<5,tclb[(i*3)+1]<<5,tclb[(i*3)+2]<<5]);
+		var rch=gtab[tclb[i*3]
+		var gch=gtab[tclb[i*3+1]
+		var bch=gtab[tclb[i*3+2]
+		//var x = new Uint16Array([gtab[tclb[i*3]],gtab[tclb[(i*3)+1]],gtab[tclb[(i*3)+2]]]);
+		
+		var ma = Math.max(rch,gch,bch);
+		//var mi = Math.min(x[0],x[1],x[2]);
+		var mi = Math.min(rch,gch,bch)
+		var mult=1;
+		if (this.gdim[i] == 31) {
+			if (ma <390 && mi < 300) {
+				this.gdim[i]=3;
+				mult=10.33;
+
+			} else if (ma <700 && mi < 500) {
+				this.gdim[i]=7;
+				mult=4.4;
+
+			} else if (ma <1700 && mi < 1000) {
+				this.gdim[i]=15;
+				mult=2.06;
+			} 
+		} else if (this.gdim[i] == 15) {
+			if (ma <390 && mi < 300) {
+				this.gdim[i]=3;
+				mult=10.33;
+			} else if (ma <700 && mi < 500) {
+				this.gdim[i]=7;
+				mult=4.4;
+			} else if (ma >1980) {
+				this.gdim[i]=31;
+			} else {
+				mult=2.06;
+			} 
+		} else if (this.gdim[i] == 7) {
+			if (ma <390 && mi < 300) {
+				this.gdim[i]=3;
+				mult=10.33;
+			} else if (ma > 1980 ) {
+				this.gdim[i]=31;
+			} else if (ma >924) {
+				this.gdim[i]=15;
+				mult=2.06;
+			} else {
+				mult=4.4;
+			}
+		} else if (this.gdim[i] == 3) {
+			if (ma > 1980 ) {
+				this.gdim[i]=31;
+			} else if (ma >924) {
+				this.gdim[i]=15;
+				mult=2.06;
+			} else if (ma>390) {
+				this.gdim[i]=7;
+				mult=4.4;
+			} else {
+				mult=10.33;
+			}
+		} 
+		rch*=mult;
+		gch*=mult;
+		bch*=mult;
+		//x[1]=x[1]*mult;
+		//x[2]=x[2]*mult;
+
+		this.fbuf[i*4]=(this.ison?(this.gdim[i]|224):224);
+		this.fbuf[1+i*4]=(bch?Math.max(x[2]>>4,1):0);
+		this.fbuf[2+i*4]=(gch?Math.max(x[2]>>4,1):0);
+		this.fbuf[3+i*4]=(rch?Math.max(x[2]>>4,1):0)
+
+	}
+
+	this.spi.write(0,0,0,0,this.fbuf,0xFF,0xFF,0xFF,0xFF);
+};
+
 gtab=new Uint16Array(256);
 gtab=new Uint16Array([0,1,2,3,4,5,6,7,8,9,11,13,15,17,19,21,23,25,27,30,33,36,39,42,45,48,51,54,58,62,66,70,74,78,82,86,91,96,101,106,111,116,121,126,132,138,144,150,156,162,168,174,181,188,195,202,209,216,223,230,238,246,254,262,270,278,286,294,303,312,321,330,339,348,357,366,376,386,396,406,416,426,436,446,457,468,479,490,501,512,523,534,546,558,570,582,594,606,618,630,643,656,669,682,695,708,721,734,748,762,776,790,804,818,832,846,861,876,891,906,921,936,951,966,982,998,1014,1030,1046,1062,1078,1094,1111,1128,1145,1162,1179,1196,1213,1230,1248,1266,1284,1302,1320,1338,1356,1374,1393,1412,1431,1450,1469,1488,1507,1526,1546,1566,1586,1606,1626,1646,1666,1686,1707,1728,1749,1770,1791,1812,1833,1854,1876,1898,1920,1942,1964,1986,2008,2030,2053,2076,2099,2122,2145,2168,2191,2214,2238,2262,2286,2310,2334,2358,2382,2406,2431,2456,2481,2506,2531,2556,2581,2606,2631,2657,2683,2709,2735,2761,2787,2813,2839,2866,2893,2920,2947,2974,3001,3028,3055,3083,3111,3139,3167,3195,3223,3251,3279,3308,3337,3366,3395,3424,3453,3482,3511,3541,3571,3601,3631,3661,3691,3721,3751,3782,3813,3844,3875,3906,3937,3968,3999,4031,4063,4095]);
 
