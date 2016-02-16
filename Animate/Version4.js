a682e85a55653cc3964e5bda0fde9bd3942c6f5c
Spence Konde <spencekonde@gmail.com>
2016-02-16 04:27:20 -0500

    Stuff

--- Animate/Version4.js
+++ Animate/Version4.js
@@ -13,7 +13,7 @@ require("ESP8266").setLog(0);
 
 
 // Parameters:
-numleds=5;
+numleds=10;
 
 //global functions
 function animate() {
@@ -34,12 +34,11 @@ function onPageRequest(req, res) {
   		res.writeHead(200,{'Access-Control-Allow-Origin':'*'});
   		res.end("OK");
   	} else {
-  		res.writeHead(400,{'Access-Control-Allow-Origin':'*'});
-  		res.end("ERROR");
+  		res.end();
   	}
   } else {
 	res.writeHead(404);
-	res.end("ERROR");	
+	res.end("NOT FOUND");	
   }
 }
 require("http").createServer(onPageRequest).listen(80);
@@ -48,13 +47,20 @@ function handleCmd(pn,q,r) {
 	try {
   if (pn=="/saveBase.cmd") {
     //lreq=a.query;
-    var temp=eval(q.eeprom)
-    leds.saveBase(temp?temp:eeprom,eval(q.address),eval(q.len);
+    if (q.index==undefined || q.index>memmap.statMax || q.index < 0) {
+    	r.write("MAX INDEX:");
+    	r.write(memmap.statMax);
+    	return 0;
+    }
+    leds.saveBase(q.index);
     return 1;
   } if (pn=="/loadBase.cmd") {
-    //lreq=a.query;
-    var temp=eval(q.eeprom)
-    leds.loadBase(temp?temp:eeprom,eval(q.address),eval(q.len);
+    if (q.index==undefined || q.index>memmap.statMax || q.index < 0) {
+    	r.write("MAX INDEX:");
+    	r.write(memmap.statMax);
+    	return 0;
+    }
+    leds.loadBase(q.index);
     return 1;
   }if (pn=="/showState.cmd") {
     //lreq=a.query;
@@ -68,25 +74,41 @@ function handleCmd(pn,q,r) {
     leds.setPixel2(q.led,0,eval(q.color),eval(q.mode),eval(q.max),eval(q.min));
     return 1;
   } else {
+  	r.writeHead(400);
+	r.write("NO CMD");
   	return 0;
   }
 	} 
 	catch (err) {
+		r.writeHead(500);
+		r.write("ERROR");
 		return 0;
 	}
 }
+
+
+var memmap={
+	slen:32,
+	rlen:40,
+	statOff:0x100,
+	statMax:14,
+	sEep:eeprom,
+	oEep:eeprom,
+	oOff:0x800
+};
 
 
 // LEDS
 gtab=new Uint16Array(256);
 gtab=new Uint16Array([0,1,2,3,4,5,6,7,8,9,11,13,15,17,19,21,23,25,27,30,33,36,39,42,45,48,51,54,58,62,66,70,74,78,82,86,91,96,101,106,111,116,121,126,132,138,144,150,156,162,168,174,181,188,195,202,209,216,223,230,238,246,254,262,270,278,286,294,303,312,321,330,339,348,357,366,376,386,396,406,416,426,436,446,457,468,479,490,501,512,523,534,546,558,570,582,594,606,618,630,643,656,669,682,695,708,721,734,748,762,776,790,804,818,832,846,861,876,891,906,921,936,951,966,982,998,1014,1030,1046,1062,1078,1094,1111,1128,1145,1162,1179,1196,1213,1230,1248,1266,1284,1302,1320,1338,1356,1374,1393,1412,1431,1450,1469,1488,1507,1526,1546,1566,1586,1606,1626,1646,1666,1686,1707,1728,1749,1770,1791,1812,1833,1854,1876,1898,1920,1942,1964,1986,2008,2030,2053,2076,2099,2122,2145,2168,2191,2214,2238,2262,2286,2310,2334,2358,2382,2406,2431,2456,2481,2506,2531,2556,2581,2606,2631,2657,2683,2709,2735,2761,2787,2813,2839,2866,2893,2920,2947,2974,3001,3028,3055,3083,3111,3139,3167,3195,3223,3251,3279,3308,3337,3366,3395,3424,3453,3482,3511,3541,3571,3601,3631,3661,3691,3721,3751,3782,3813,3844,3875,3906,3937,3968,3999,4031,4063,4095]);
 var leds = {};
+leds.map=memmap;
 leds.spi=SPI1;
 leds.num=numleds;
 leds.afr=0;
 leds.fbuf=new Uint8Array(numleds*4);
 leds.buff=new Uint8Array(numleds*3);
-leds.tbuf=new Uint8Array(numleds*3);
+leds.tbuf=new Uint8ClampedArray(numleds*3);
 leds.t=new Int8Array(numleds*3);
 leds.tm=new Uint8Array(numleds*3);
 leds.ti=new Int8Array(numleds*3);
@@ -103,6 +125,8 @@ leds.ison=1;
 leds.animode=0;
 leds.aniframe=0;
 leds.anilast=0;
+leds.aniaddr=0;
+leds.zz="\x00\x00"
 
 leds.dotwinkle = function () {
 	var t=this.t;
@@ -117,14 +141,15 @@ leds.dotwinkle = function () {
 			this.animode=0;
 			this.anilast=0;
 			this.aniframe=0;
-			this.overlay=new Uint8Array(this.num*3);
+			leds.aniaddr=0;
+			this.overlay.fill(0);
 		} else {
-			this.overlay=this.animation[this.aniframe++];
+			this.overlay=this.map.oEep.read(this.aniaddr+this.map.slen*this.aniframe++,this.num*3);
 		}
 	}
 	for (var i=0;i<this.num*3;i++){
-		var c=b[i]
-		b[i]+=E.clip(z[i]-c,-1,1));
+		var c=b[i];
+		b[i]+=E.clip(z[i]-c,-1,1);
 		var mode=tm[i];
 		var mo=mode&0x0F;
 		var pr=mode>>4;
@@ -132,7 +157,7 @@ leds.dotwinkle = function () {
 			var n=Math.random(); //3ms
 			var th=(pr+1)/32;
       			if (n<0.5+th){ //8ms
-      				if(n<=(0.5-th) && t[i]>ta[i]){t[i]--;}
+      				if(n<=(0.5-th) && t[i]>ti[i]){t[i]--;}
       			} else {
       				if (t[i]<ta[i]){t[i]++;}
       			}
@@ -143,6 +168,8 @@ leds.dotwinkle = function () {
 					tm[i]=mode^128;
 				}
         		}
+		} else {
+			if (t[i]!==0){if(t[i]>0){t[i]--;} else {t[i]++;}}
 		}
 		leds.tclb[i]=c+(c?t[i]:0)+o[i]; //10ms
 	}
@@ -152,7 +179,7 @@ leds.dotwinkle = function () {
 leds.setAll= function (color,tmode,tmax,tmin) {
 	for (var i=0;i<this.num;i++) {
 		for (j=0;j<3;j++){
-			this.t[3*i+j]=0;
+			//this.t[3*i+j]=0;
 			this.tbuf[3*i+j]=color[j];
 			if (tmode) {
 				this.tm[3*i+j]=tmode[j];
@@ -163,21 +190,19 @@ leds.setAll= function (color,tmode,tmax,tmin) {
 	}
 };
 
-leds.loadBase = function (eep,addr,len) {
-  	len=len?len:this.num*3; 
-	this.tbuf=eep.read(addr,this.num*3);
-	this.t.fill(0);
-	this.tm=new Uint8Array(eep.read((addr+len),this.num*3));
-	this.ti=new Int8Array(eep.read((addr+len*2),this.num*3));
-	this.ta=new Int8Array(eep.read((addr+len*3),this.num*3));
+leds.loadBase = function (index) {
+	var s=this.map.slen;
+	var addr=this.map.statOff+(4*index*s);
+	this.tbuf=this.map.sEep.read(addr,this.num*3);
+	this.tm=this.map.sEep.read((addr+s),this.num*3);
+	this.ti=new Int8Array(this.map.sEep.read((addr+s*2),this.num*3));
+	this.ta=new Int8Array(this.map.sEep.read((addr+s*3),this.num*3));
 };
 
-leds.saveBase = function (eep,addr,len) {
-  	len=len?len:this.num*3; 
-	eep.write(addr,this.tbuf);
-	eep.write(addr+len,this.tm);
-	eep.write(addr+len*2,this.ti);
-	eep.write(addr+len*3,this.ta);
+leds.saveBase = function (index) {
+	var s=this.map.slen;
+	var addr=this.map.statOff+(4*index*s);
+	this.map.sEep.write(addr,E.toString(this.tbuf)+leds.zz+E.toString(this.tm)+leds.zz+E.toString(this.ti)+leds.zz+E.toString(this.ta)+leds.zz);
 };
 
 
@@ -185,6 +210,18 @@ leds.setPixel = function (x, y, color) {
 	this.tbuf[x*3]=color[0];
 	this.tbuf[x*3+1]=color[1];
 	this.tbuf[x*3+2]=color[2];
+};
+
+leds.adjPixel=function (led,color) {
+	this.tbuf[led*3]+=color[0];
+	this.tbuf[led*3+1]+=color[1];
+	this.tbuf[led*3+2]+=color[2];
+};
+
+leds.adjAll=function (color) {
+	for (var i=0;i<this.num;i++){
+		this.adjPixel(i,color);
+	}
 };
 
 leds.setPixel2 = function (x, y, color,mode,mintwi,maxtwi) {
