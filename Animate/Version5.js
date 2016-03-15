947b5e69a8eb6618297b4dfa0d420818ab384b69
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2016-03-15 13:14:25 -0400

    Update Version5.js
    Refactor HTTP code to hopefully make the code smaller.

--- Animate/Version5.js
+++ Animate/Version5.js
@@ -64,88 +64,46 @@ var CORS={'Access-Control-Allow-Origin':'*'};
 
 function onPageRequest(req, res) {
   var a = url.parse(req.url, true);
-  if (a.pathname.split(".")[1]=="cmd"){
-  	if (handleCmd(a.pathname,a.query,res)) {
-  		res.writeHead(200,CORS);
-  	} 
-  	res.end();
+  var resu = handleCmd(a.pathname,a.query,res);
+  if (resu) {
+  	res.writeHead(resu,CORS);
+  	if (resu==200) {res.write("OK");}
+  	else if (resu==404) {res.write("Not Found");}
   } else {
-	res.writeHead(404,CORS);
-	res.end("NOT FOUND");	
+  	res.writeHead(500,CORS);
+  	res.write("ERROR");
   }
+  res.end();
 }
 require("http").createServer(onPageRequest).listen(80);
 
 function handleCmd(pn,q,r) {
 	try {
-  if (pn=="/save.cmd") {
-    //lreq=a.query;
-    if (q.index==undefined || q.index>memmap.statMax || q.index < 0) {
-    	r.write("MAX INDEX:");
-    	r.write(memmap.statMax);
-    	return 0;
-    }
-    leds.save(q.index);
-    r.write("OK");
-    return 1;
-  } else if (pn=="/load.cmd") {
-    if (q.index==undefined || q.index>memmap.statMax || q.index < 0) {
-    	r.write("MAX INDEX:");
-    	r.write(memmap.statMax);
-    	return 0;
-    }
-    if (!leds.load(q.index)){
-    	r.writeHead(404,CORS);
-    	r.write("No such pattern");
-    	return 0;
-    }
-    r.write("OK");
-    return 1;
-  }else if (pn=="/delete.cmd") {
-    if (q.index==undefined || q.index>memmap.statMax || q.index < 0) {
-    	r.write("MAX INDEX:");
-    	r.write(memmap.statMax);
-    	return 0;
-    }
-    if (!leds.delete(q.index)){
-    	r.writeHead(404,CORS);
-    	r.write("No such pattern");
-    	return 0;
-    }
-    r.write("OK");
-    return 1;
-  } else if (pn=="/showState.cmd") {
-    //lreq=a.query;
-    r.write(JSON.stringify({"base":leds.tbuf,"twinkle":[leds.tm,leds.ti,leds.ta]}));
-    return 1;
-  } else if (pn=="/setScene.cmd") {
-    //lreq=a.query;
-    leds.setScene(q.scene);
-    r.write("OK");
-    return 1;
-  }else if (pn=="/event.cmd") {
-    //lreq=a.query;
-    leds.sceneVect(eval(q.vector));
-    r.write("OK");
-    return 1;
-  }else if (pn=="/setAll.cmd") {
-    //lreq=a.query;
-    leds.setAll(eval(q.color),eval(q.mode),eval(q.max),eval(q.min));
-    r.write("OK");
-    return 1;
-  }else if (pn=="/setPixel.cmd") {
-    leds.setPixel2(q.led,0,eval(q.color),eval(q.mode),eval(q.max),eval(q.min));
-    r.write("OK");
-    return 1;
-  } else {
-  	r.writeHead(404,CORS);
-	r.write("NO CMD");
-  	return 0;
-  }
-	} 
-	catch (err) {
-		r.writeHead(500,CORS);
-		r.write("ERROR");
+		if ((q.index!=undefined)&&( q.index>memmap.statMax || q.index < 0)) {
+			r.write("MAX INDEX: "+memmap.statMax);
+		    return 400;
+		}
+		if (pn=="/save.cmd") {
+		    return (leds.save(q.index))?200:0;
+	  	} else if (pn=="/load.cmd") {
+		    return leds.load(q.index))?200:404;
+  		}else if (pn=="/delete.cmd") {
+		    return leds.delete(q.index)?200:404;
+		} else if (pn=="/showState.cmd") {
+    		r.write(JSON.stringify({"base":leds.tbuf,"twinkle":[leds.tm,leds.ti,leds.ta]}));
+    		return 200;
+  		} else if (pn=="/setScene.cmd") {
+    		return 1leds.setScene(q.scene)?200:0;
+  		}else if (pn=="/event.cmd") {
+    		return leds.sceneVect(eval(q.vector))?200:0;
+  		}else if (pn=="/setAll.cmd") {
+    		return leds.setAll(eval(q.color),eval(q.mode),eval(q.max),eval(q.min))?200:0;
+  		}else if (pn=="/setPixel.cmd") {
+    		return leds.setPixel2(q.led,0,eval(q.color),eval(q.mode),eval(q.max),eval(q.min))?200:0;
+  		} else {
+  			return 404;
+		}
+	} catch (err) {
 		return 0;
 	}
 }
