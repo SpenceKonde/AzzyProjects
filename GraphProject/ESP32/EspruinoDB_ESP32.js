c98677d6fd6e5a8fa05ec82664595d605597e419
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2018-06-06 08:14:12 -0400

    asd

--- GraphProject/ESP32/EspruinoDB_ESP32.js
+++ GraphProject/ESP32/EspruinoDB_ESP32.js
@@ -1,21 +1,24 @@
 var http = require("http");
 
 
+
 function startup() {
 	initDB();
-	E.setTimeZone();
+    Serial2.setup(115200,{rx:16, tx:17}); //Serial for RF interface
+    Serial2.on('data',function(data){handleSerial(data);}); //
+	E.setTimeZone(-5);
 	fetchTime();
-  require("http").createServer(onPageRequest).listen(80);
+    require("http").createServer(onPageRequest).listen(7348);
 }
 
 function fetchTime() {
-	require("http").get("http://raspi/getTime.shtml", function(res) {
-  var contents = "";
-  res.on('data', function(data) { contents += data; });
-  res.on('close', function() { setTime(contents); });
-}).on('error', function(e) {
-  console.log("ERROR", e);
-});
+	require("http").get("http://drazzy.com/getTime.shtml", function(res) {
+  		var contents = "";
+  		res.on('data', function(data) { contents += data; });
+  		res.on('close', function() { setTime(contents); });
+	}).on('error', function(e) {
+  		console.log("ERROR", e);
+	});
 }
 
 var CORS={'Access-Control-Allow-Origin':'*'};
@@ -66,3 +69,48 @@ function initDB() {
 function onInit() {
 	setTimeout("startup()",5000)
 }
+
+var RFAddresses=[31];
+
+function handleSerial(data) {
+  console.log(data);
+  var rcv={};
+  if (data.charAt(0)=="=") {
+    rcv.version=2;
+  } else {
+    rcv.version=1;
+  }
+  v=parseInt(data.substr(1,2),16)>>6;
+  l=(v?(v==3?31:v==2?15:7):4);
+  rcv.data=new Uint8Array(l);
+  for (i=0;i<l;i++){
+    rcv.data[i]=parseInt(data.substr(2*i+1,2),16);
+  }
+  rcv.length=l;
+  rcv.destaddress=rcv.data[0]&0x3F;
+  if (RFAddresses[0]==0 || RFAddresses.indexOf(rcv.destaddress)!=-1) { //is the packet for this device, or is this device universal listener?
+    processPacket(rcv);
+  }
+}
+
+function processPacket(rcvpkt) {
+  if (rcvpkt.version==1) {
+    processPacket_V1(rcvpkt);
+  } else {
+    processPacket_V2(rcvpkt);
+  }
+}
+
+
+function processPacket_V1(r) {
+  if(r.destaddress==31) {
+  	if (r.data[1]==225) {
+  	  updateSwitch(r.data[2],r.data[3]);
+  	}
+  }
+}
+
+function processPacket_V2(rcvpkt) {
+  
+}
+
