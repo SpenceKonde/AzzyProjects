8a084d693d3b82677d75024a65fdd3eaf3287ab2
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-01-03 19:30:36 -0500

    Update load.php

--- Animate/WebControlPanel/load.php
+++ Animate/WebControlPanel/load.php
@@ -1,10 +1,10 @@
 <?php 
 if ($_GET['index'] > -1 && $_GET['index']<101) {
-http_get("http://192.168.2.35/load.cmd?" . $_GET['index']);
-http_get("http://192.168.2.36/load.cmd?" . $_GET['index']);
-http_get("http://192.168.2.37/load.cmd?" . $_GET['index']);
-http_get("http://192.168.2.38/load.cmd?" . $_GET['index']);
-http_get("http://192.168.2.39/load.cmd?" . $_GET['index']);
-http_get("http://192.168.2.40/load.cmd?" . $_GET['index']);
-http_get("http://192.168.2.41/load.cmd?" . $_GET['index']);
+http_get("http://pong1/load.cmd?" . $_GET['index']);
+http_get("http://pong2/load.cmd?" . $_GET['index']);
+http_get("http://pong3/load.cmd?" . $_GET['index']);
+http_get("http://pong4/load.cmd?" . $_GET['index']);
+http_get("http://pong5/load.cmd?" . $_GET['index']);
+http_get("http://pong6/load.cmd?" . $_GET['index']);
+http_get("http://pong7/load.cmd?" . $_GET['index']);
 }
