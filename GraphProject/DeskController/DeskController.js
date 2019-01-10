4a37fee061f927082ed7ff2fe720cc25bc143f03
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-01-10 00:32:05 -0500

    drain, fix bug with history, stubs for VFD update

--- GraphProject/DeskController/DeskController.js
+++ GraphProject/DeskController/DeskController.js
@@ -13,65 +13,65 @@
 	UPMOD={Clock:0,Sensors:0,Webserver:0,History:0};
 	Clock = require("clock").Clock;
 	// I2C devices
-    I2C1.setup({scl:B8,sda:B9});
+	I2C1.setup({scl:B8,sda:B9});
     BME680=require("BME680").connectI2C(I2C1); //T/P/RH/AQI sensor. 
     TCS=require("TCS3472x").connect(I2C1,16,1); //light color sensor.
     FRAM=require("AT24").connect(I2C1,0,256,0); //256 kbit FRAM for storing presets and other data that may change frequently
     //Nixie Clock
-	Serial5.setup(115200,{tx:C12});
-	Nixie=require("SmartNixie").connect(Serial5,6);
+    Serial5.setup(115200,{tx:C12});
+    Nixie=require("SmartNixie").connect(Serial5,6);
 	//Keypad
 	KeyPad=require("KeyPad").connect([C0, C1, C2, C3, C4],[A0,A1,B0,B1], function(e) {onKey("AL741B0852#R963*ECDU"[e]);});
 	//WIZnet
-    SPI3.setup({sck:B3,mosi:B5,miso:B4});
+	SPI3.setup({sck:B3,mosi:B5,miso:B4});
 	Eth=require("WIZnet").connect(SPI3,B2);
 	Eth.setIP();
 	//VFD
-    Serial1.setup(19200,{tx:B6});
+	Serial1.setup(19200,{tx:B6});
 	digitalWrite(B7,0); // Turn off the power for the VFD. 
 	//AzzyRF
-    Serial3.setup(115200,{tx:C10,rx:C11});
+	Serial3.setup(115200,{tx:C10,rx:C11});
     //
     //initialize variables
-	fargosturl="http://192.168.2.21/fargostatus.php";
-	dateurl="http://drazzy.com/time.shtml";
-	fargourl="http://192.168.2.14/api/relay/";
-	blankpreset="\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF";
-	colors="BLUECOOLWARMYELLRED ";
-	fargostrs="Colored,White,Wizard,Tentacle,Desk,Micro,Fan,Under";
-	nixs=0;
-	nixr=0;
+    fargosturl="http://192.168.2.21/fargostatus.php";
+    dateurl="http://drazzy.com/time.shtml";
+    fargourl="http://192.168.2.14/api/relay/";
+    blankpreset="\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF";
+    colors="BLUECOOLWARMYELLRED ";
+    fargostrs="Colored,White,Wizard,Tentacle,Desk,Micro,Fan,Under";
+    nixs=0;
+    nixr=0;
 
-	head={'Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*', 'Connection':'close'};
-	htypes=["Temp","RH","Pressure","AirQual","Clear","Red","Green","Blue"];
-	presets=[new Float32Array([0.0,0.0,0.0,0.0,0.0]),new Float32Array([0.9,1,0.6,0,0]),new Float32Array([0,0.6,1,0.8,0.4]),new Float32Array([0.5,0.5,0.5,0.5,0.5])];
-	prenames=["OFF","COLD WHITE","VERY WARM","HALF AND HALF"];
-	STATUS={}; 
-	STATUS.Fargo=new Uint8Array(8);
-	STATUS.Nixie={on:0,mode:0};
+    head={'Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*', 'Connection':'close'};
+    htypes=["Temp","RH","Pressure","AirQual","Clear","Red","Green","Blue"];
+    presets=[new Float32Array([0.0,0.0,0.0,0.0,0.0]),new Float32Array([0.9,1,0.6,0,0]),new Float32Array([0,0.6,1,0.8,0.4]),new Float32Array([0.5,0.5,0.5,0.5,0.5])];
+    prenames=["OFF","COLD WHITE","VERY WARM","HALF AND HALF"];
+    STATUS={}; 
+    STATUS.Fargo=new Uint8Array(8);
+    STATUS.Nixie={on:0,mode:0};
     STATUS.LEDs=[0.0,0.0,0.0,0.0,0.0];
-	STATUS.Temp=0;
-	STATUS.RH=0;
-	STATUS.Pressure=0;
-	STATUS.AirQual=0;
-	HISTORY={};
-	HISTORY.Temp=new Float32Array(48);
-	HISTORY.RH=new Float32Array(48);
-	HISTORY.Pressure=new Float32Array(48);
-	HISTORY.AirQual=new Float32Array(48);
-	HISTORY.Clear=new Uint16Array(48);
-	HISTORY.Red=new Uint16Array(48);
-	HISTORY.Green=new Uint16Array(48);
-	HISTORY.Blue=new Uint16Array(48);
+    STATUS.Temp=0;
+    STATUS.RH=0;
+    STATUS.Pressure=0;
+    STATUS.AirQual=0;
+    HISTORY={};
+    HISTORY.Temp=new Float32Array(48);
+    HISTORY.RH=new Float32Array(48);
+    HISTORY.Pressure=new Float32Array(48);
+    HISTORY.AirQual=new Float32Array(48);
+    HISTORY.Clear=new Uint16Array(48);
+    HISTORY.Red=new Uint16Array(48);
+    HISTORY.Green=new Uint16Array(48);
+    HISTORY.Blue=new Uint16Array(48);
     INTERVALS={webserver:-1,sensors:-1,nixie:-1,history:-1,fargo:-1,date:-1,activity:-1,fivemin:-1}; //webserver, sensors, fargo, date, activity, 
-	menu=[2,4,presets.length-1];
+    menu=[2,4,presets.length-1];
 	//if this pin is grounded, we return before actually kicking everything off for maintenance mode. 
     //if (1) {return;}
-	INTERVALS.webserver=setTimeout("var WebServer=require('http').createServer(processrequest).listen(80);",15000);
+    INTERVALS.webserver=setTimeout("var WebServer=require('http').createServer(processrequest).listen(80);",15000);
     setTimeout("loadPresets();",1000);
-	INTERVALS.sensors=setInterval("readSensors()",15000);
-	setTimeout("loadHistory()",3000);
-	setTimeout("onHalfHour()",15000);
+    INTERVALS.sensors=setInterval("readSensors()",15000);
+    setTimeout("loadHistory()",3000);
+    setTimeout("onHalfHour()",15000);
 	//setTimeout("setInterval(function(){uplcd();},30000);",15000);
 	setTimeout("INTERVALS.fargo=setInterval(getFargoStatus,30000);",20000);
 	setTimeout("INTERVALS.nixie=setInterval(upNixie,30000);",30000);
@@ -87,13 +87,13 @@
 function onHalfHour() {
 	if (INTERVALS.history != -1) { //otherwise, interval hasn't been set yet. 
 		INTERVALS.history=-1;
-		updateHistory();
-        saveHistory();
-	} 
-	var mins=clk.getDate().getMinutes();
+	updateHistory();
+	saveHistory();
+} 
+var mins=clk.getDate().getMinutes();
 	var m=(30-mins%5); //TEST MODE
-    m=m*60;
-    m=m-clk.getDate().getSeconds(); 
+	m=m*60;
+	m=m-clk.getDate().getSeconds(); 
 	INTERVALS.history=setTimeout(onHalfHour,m*1000); //now we set t
 }
 
@@ -111,12 +111,12 @@ function onActivityOn() {
 }
 
 function onActivityOff() {
-    digitalWrite(B7,0);
-    if (STATUS.clear < 2000) {
-	STATUS.Nixie.on=0;
-	upNixie(); 
-    }
-    INTERVALS.Activity=-1;
+	digitalWrite(B7,0);
+	if (STATUS.clear < 2000) {
+		STATUS.Nixie.on=0;
+		upNixie(); 
+	}
+	INTERVALS.Activity=-1;
 }
 
 function loadHistory() {
@@ -132,7 +132,7 @@ function loadHistory() {
 	a+=48*4;
 	t=new Uint8Array(FRAM.read(a,48*4));
 	HISTORY.AirQual=new Float32Array(t.buffer);
-	a+=48*2;
+	a+=48*4;
 	t=new Uint8Array(FRAM.read(a,48*2));
 	HISTORY.Clear=new Uint16Array(t.buffer);
 	a+=48*2;
@@ -147,50 +147,50 @@ function loadHistory() {
 }
 function saveHistory() {
 	var a=2048;
-   var t=new Uint8Array(HISTORY.Temp.buffer);
-   FRAM.write(a,t);
-   a+=48*4;
-   t=new Uint8Array(HISTORY.RH.buffer);
-   FRAM.write(a,t);
-   a+=48*4;
-   t=new Uint8Array(HISTORY.Pressure.buffer);
-   FRAM.write(a,t);
-   a+=48*4;
-   t=new Uint8Array(HISTORY.AirQual.buffer);
-   FRAM.write(a,t);
-   a+=48*2;
-   t=new Uint8Array(HISTORY.Clear.buffer);
-   FRAM.write(a,t);
-   a+=48*2;
-   t=new Uint8Array(HISTORY.Red.buffer);
-   FRAM.write(a,t);
-   a+=48*2;
-   t=new Uint8Array(HISTORY.Green.buffer);
-   FRAM.write(a,t);
-   a+=48*2;
-   t=new Uint8Array(HISTORY.Blue.buffer);
-   FRAM.write(a,t);
+	var t=new Uint8Array(HISTORY.Temp.buffer);
+	FRAM.write(a,t);
+	a+=48*4;
+	t=new Uint8Array(HISTORY.RH.buffer);
+	FRAM.write(a,t);
+	a+=48*4;
+	t=new Uint8Array(HISTORY.Pressure.buffer);
+	FRAM.write(a,t);
+	a+=48*4;
+	t=new Uint8Array(HISTORY.AirQual.buffer);
+	FRAM.write(a,t);
+	a+=48*4;
+	t=new Uint8Array(HISTORY.Clear.buffer);
+	FRAM.write(a,t);
+	a+=48*2;
+	t=new Uint8Array(HISTORY.Red.buffer);
+	FRAM.write(a,t);
+	a+=48*2;
+	t=new Uint8Array(HISTORY.Green.buffer);
+	FRAM.write(a,t);
+	a+=48*2;
+	t=new Uint8Array(HISTORY.Blue.buffer);
+	FRAM.write(a,t);
 }
 
 function updateHistory() {
-   for (var i=0;i<47;i++) {
-   	HISTORY.Temp[i]=HISTORY.Temp[i+1];
-   	HISTORY.RH[i]=HISTORY.RH[i+1];
-   	HISTORY.Pressure[i]=HISTORY.Pressure[i+1];
-   	HISTORY.AirQual[i]=HISTORY.AirQual[i+1];
-   	HISTORY.Clear[i]=HISTORY.Clear[i+1];
-   	HISTORY.Red[i]=HISTORY.Red[i+1];
-   	HISTORY.Green[i]=HISTORY.Green[i+1];
-   	HISTORY.Blue[i]=HISTORY.Blue[i+1];
-   }
-   HISTORY.Temp[47]=STATUS.Temp;
-   HISTORY.RH[47]=STATUS.RH;
-   HISTORY.Pressure[47]=STATUS.Pressure;
-   HISTORY.AirQual[47]=STATUS.AirQual;
-   HISTORY.Clear[47]=STATUS.Light.clear;
-   HISTORY.Red[47]=STATUS.Light.red;
-   HISTORY.Green[47]=STATUS.Light.green;
-   HISTORY.Blue[47]=STATUS.Light.blue;
+	for (var i=0;i<47;i++) {
+		HISTORY.Temp[i]=HISTORY.Temp[i+1];
+		HISTORY.RH[i]=HISTORY.RH[i+1];
+		HISTORY.Pressure[i]=HISTORY.Pressure[i+1];
+		HISTORY.AirQual[i]=HISTORY.AirQual[i+1];
+		HISTORY.Clear[i]=HISTORY.Clear[i+1];
+		HISTORY.Red[i]=HISTORY.Red[i+1];
+		HISTORY.Green[i]=HISTORY.Green[i+1];
+		HISTORY.Blue[i]=HISTORY.Blue[i+1];
+	}
+	HISTORY.Temp[47]=STATUS.Temp;
+	HISTORY.RH[47]=STATUS.RH;
+	HISTORY.Pressure[47]=STATUS.Pressure;
+	HISTORY.AirQual[47]=STATUS.AirQual;
+	HISTORY.Clear[47]=STATUS.Light.clear;
+	HISTORY.Red[47]=STATUS.Light.red;
+	HISTORY.Green[47]=STATUS.Light.green;
+	HISTORY.Blue[47]=STATUS.Light.blue;
 
 }
 
@@ -211,10 +211,10 @@ function loadPresets() {
 	prenames=["OFF"];
 	var i=1;
 	while ((FRAM.read(32*(i-1),1) != 255)&&i<=32) { //read presets from fram
-		 prenames[i]=E.toString(FRAM.read(32*(i-1),20));
-		 var temp=FRAM.read(32*(i-1)+20,5);
-		 presets[i]=new Float32Array([temp[0]/100.0,temp[1]/100.0,temp[2]/100.0,temp[3]/100.0,temp[4]/100.0]);
-		 i++;
+		prenames[i]=E.toString(FRAM.read(32*(i-1),20));
+		var temp=FRAM.read(32*(i-1)+20,5);
+		presets[i]=new Float32Array([temp[0]/100.0,temp[1]/100.0,temp[2]/100.0,temp[3]/100.0,temp[4]/100.0]);
+		i++;
 	}
 }
 
@@ -228,7 +228,7 @@ function savePresets() {
 		while (tstring.length<20) {
 			tstring+=" ";
 		}
-	    FRAM.write((i-1)*32,tstring);
+		FRAM.write((i-1)*32,tstring);
 	} 
 	//clean up any deleted functions
 	var t=presets.length-1; 
@@ -286,8 +286,8 @@ function getFargoStatus() {
 	var fargost="";
 	require("http").get(fargosturl, function(res) {
 		res.on('data',function (data) {fargost+=data;});
-		res.on('close',function() {var tfs=JSON.parse(fargost); for (var i=0;i<8;i++) { STATUS.Fargo[i]=tfs.relaystate[i].state;} /*if(MnuS==3){uplcd(1);}*/});
-	});
+	res.on('close',function() {var tfs=JSON.parse(fargost); for (var i=0;i<8;i++) { STATUS.Fargo[i]=tfs.relaystate[i].state;} /*if(MnuS==3){uplcd(1);}*/});
+});
 }
 
 function setFargo(relay,state) {
@@ -297,8 +297,8 @@ function setFargo(relay,state) {
 			if(this.code==200) {
 				STATUS.Fargo[relay]=state;
 			} else {
-              console.log(this.code);
-            }
+				console.log(this.code);
+			}
 		});
 	});
 }
@@ -332,16 +332,35 @@ function upNixie() {
 
 //END OF FARGO CODE
 
+//START VFD CODE 
+function uplcd() {
+	if (INTERVALS.activity==-1) {
+		return;
+	}
+
+}
+function drawFargo(pg) {
+
+}
+function drawLights() {
+
+}
+function drawStatus() {
+	
+}
+
+//END OF VFD CODE
+
 //START SENSOR READING CODE
 
 function readSensors() {
 	BME680.perform_measurement();
-    var t=BME680.get_sensor_data();
-    STATUS.RH=t.humidity;
-    STATUS.Temp=t.temperature;
-    STATUS.Pressure=t.pressure;
-  STATUS.AirQual=t.gas_resistance;
-  STATUS.Light=TCS.getValue();
+	var t=BME680.get_sensor_data();
+	STATUS.RH=t.humidity;
+	STATUS.Temp=t.temperature;
+	STATUS.Pressure=t.pressure;
+	STATUS.AirQual=t.gas_resistance;
+	STATUS.Light=TCS.getValue();
 }
 
 
@@ -363,35 +382,23 @@ function onRFMessage(msg) {
 
 //START HTTP INTERFACE CODE
 
-function writeTypedHistory(res,tdx) {
-	var typen = htypes[tdx];
-	res.write((tdx === 0) ? '{"' : ',"');
-	res.write(typen);
-	res.write('":');
-	res.write(getTypesHistString(tdx));
-  	if (++tdx < htypes.length) {
-    	setTimeout(writeTypedHistory,100,res,tdx);
-  	} else {
-    	res.write('}');
-    	res.end();
-    }
-}
 
-function getTypesHistString(tdx) {
+
+function getTypedHistString(tdx) {
 	var ret="";
 	var typen = htypes[tdx];
 	if (tdx<4){
-  		var tstr="[";
-  		for (var i=0;i<48;i++) {
+		var tstr="[";
+		for (var i=0;i<48;i++) {
 			tstr=tstr+HISTORY[typen][i].toFixed(1);
-  			if (i !=47) {
-  				tstr+=",";
-  			} 
-  		}
-  		tstr+="]";
-  		return tstr;
+			if (i !=47) {
+				tstr+=",";
+			} 
+		}
+		tstr+="]";
+		return tstr;
 	} else {
-  		return JSON.stringify(HISTORY[typen]);
+		return JSON.stringify(HISTORY[typen]);
 	}
 }
 
@@ -403,73 +410,80 @@ function processrequest(req, res) {
 		var path=par.pathname; 
 		path=path.split('/');
 		path=path[1];
-	    switch(path) {
-	    	case "status.json":
-	    		var r='{"lamp":{';
-				for (var i=0;i<5;i++) {
-					r+='"'+colors.substr(i*4,4)+'":'+STATUS.LEDs[i].toFixed(2);
-					if (i<4) {r+=",";}
-				}
-				r+='},\n"sensors":{"RH":'+STATUS.RH.toFixed(1)+',"Temp":'+STATUS.Temp.toFixed(1)+',"Pressure":'+STATUS.Pressure.toFixed(2)+',"Air Quality":'+STATUS.AirQual.toFixed(2)+'},\n"Light":'+JSON.stringify(STATUS.Light)+'}';
+		switch(path) {
+			case "status.json":
+			var r='{"lamp":{';
+			for (var i=0;i<5;i++) {
+				r+='"'+colors.substr(i*4,4)+'":'+STATUS.LEDs[i].toFixed(2);
+				if (i<4) {r+=",";}
+			}
+			r+='},\n"sensors":{"RH":'+STATUS.RH.toFixed(1)+',"Temp":'+STATUS.Temp.toFixed(1)+',"Pressure":'+STATUS.Pressure.toFixed(2)+',"Air Quality":'+STATUS.AirQual.toFixed(2)+'},\n"Light":'+JSON.stringify(STATUS.Light)+'}';
+			res.writeHead(200,head);
+			res.write(r);
+			res.end();
+			break;
+			case "history.json":
+			res.writeHead(200,head);
+			var n=0;
+			res.on('drain',function(){
+				res.write('"'+htypes[n]+'"');
+				res.write(":")
+				res.write(getTypedHistString(n++));
+				if (n==8) {res.end("}")} else {res.write(',\r\n');}
+			}
+			res.write("{");
+			break;
+			case "usermsg.cmd":
+			if (query.msg && query.msg.length > 1) {
+				usrmsg=query.msg;
+				MnuS=10;
+				setTimeout("uplcd();",100);
 				res.writeHead(200,head);
-				res.write(r);
-                res.end();
-	    		break;
-	    	case "history.json":
-				res.writeHead(200,head);
-				setTimeout(writeTypedHistory,10,res,0);
-	    		break;
-	    	case "usermsg.cmd":
-	    		if (query.msg && query.msg.length > 1) {
-					usrmsg=query.msg;
-					MnuS=10;
-					setTimeout("uplcd();",100);
-	    			res.writeHead(200,head);
-	    			res.write("{status:\"ok\",message:\""+query.msg+"\"}");
-	    		} else {
-					res.writeHead(400,head);
-					res.write("{status:\"error\",error:\"No message supplied\"}");
+				res.write("{status:\"ok\",message:\""+query.msg+"\"}");
+			} else {
+				res.writeHead(400,head);
+				res.write("{status:\"error\",error:\"No message supplied\"}");
+			}
+			res.end();
+			break;
+			case "lamp.cmd":
+			STATUS.LEDs[0]=q.BLUE===undefined ? 0:E.clip(q.BLUE,0,1);
+			STATUS.LEDs[1]=q.COOL===undefined ? 0:E.clip(q.COOL,0,1);
+			STATUS.LEDs[2]=q.WARM===undefined ? 0:E.clip(q.WARM,0,1);
+			STATUS.LEDs[3]=q.YELL===undefined ? 0:E.clip(q.YELL,0,1);
+			STATUS.LEDs[4]=q.RED===undefined ? 0:E.clip(q.RED,0,1);
+			setTimeout("upled();",100);
+			res.writeHead(200,head);
+			res.write("{status:\"ok\",message:\"lamp state set\"}");
+			res.end();
+			break;
+			case "code.run":
+			if (query.code) {
+				var x="";
+				try {
+					x=eval(query.code);
+					res.writeHead(200,head);
+					res.write("{status:\"ok\",result:\""+x+"\"}");
 				}
-				res.end();
-	    		break;
-	    	case "lamp.cmd":
-	    		STATUS.LEDs[0]=q.BLUE===undefined ? 0:E.clip(q.BLUE,0,1);
-				STATUS.LEDs[1]=q.COOL===undefined ? 0:E.clip(q.COOL,0,1);
-				STATUS.LEDs[2]=q.WARM===undefined ? 0:E.clip(q.WARM,0,1);
-				STATUS.LEDs[3]=q.YELL===undefined ? 0:E.clip(q.YELL,0,1);
-				STATUS.LEDs[4]=q.RED===undefined ? 0:E.clip(q.RED,0,1);
-				setTimeout("upled();",100);
-	    		res.writeHead(200,head);
-	    		res.write("{status:\"ok\",message:\"lamp state set\"}");
-	    		res.end();
-	    		break;
-	    	case "code.run":
-	    		if (query.code) {
-	    			var x="";
-	    			try {
-	    				x=eval(query.code);
-	    				res.writeHead(200,head);
-	    				res.write("{status:\"ok\",result:\""+x+"\"}");
-	    			}
-	    			catch (err) {
-	    				res.writeHead(500,head);
-	    				res.write("{status:\"error\",error:\""+err.message+"\"}");
-	    			}
-				} else {
-					res.writeHead(400,head);
-					res.write("{status:\"error\",error:\"No code supplied.\"}");
+				catch (err) {
+					res.writeHead(500,head);
+					res.write("{status:\"error\",error:\""+err.message+"\"}");
 				}
-				res.end();
-	    		break;
-	    	default:
-	    		res.writeHead(404,head);
-				res.write("{status:\"error\",error:\"unknown command\"}");
-				res.end();
-	    }
+			} else {
+				res.writeHead(400,head);
+				res.write("{status:\"error\",error:\"No code supplied.\"}");
+			}
+			res.end();
+			break;
+			default:
+			res.writeHead(404,head);
+			res.write("{status:\"error\",error:\"unknown command\"}");
+			res.end();
+		}
 	}
 	catch (err){
 		res.writeHead(500,head);
-	    res.write("{status:\"error\",error:\""+err.message+"\"}");
+		res.write("{status:\"error\",error:\""+err.message+"\"}");
 		res.end();
 	}
 }
@@ -481,16 +495,16 @@ function processrequest(req, res) {
 //Start of timing related code 
 
 function getTStr(sep){
-  var hrs=clk.getDate().getHours();
-  var mins=clk.getDate().getMinutes();
-  return (hrs>9?hrs:" "+hrs)+sep+(mins>9?mins:"0"+mins);
+	var hrs=clk.getDate().getHours();
+	var mins=clk.getDate().getMinutes();
+	return (hrs>9?hrs:" "+hrs)+sep+(mins>9?mins:"0"+mins);
 }
 
 function getDstr(){
-  var mon=clk.getDate().getMonth()+1;
-  var day=clk.getDate().getDate();
-  var year=clk.getDate().getFullYear();
-  return (mon+"/"+day+"/"+year);
+	var mon=clk.getDate().getMonth()+1;
+	var day=clk.getDate().getDate();
+	var year=clk.getDate().getFullYear();
+	return (mon+"/"+day+"/"+year);
 }
 
 function getDate() {
